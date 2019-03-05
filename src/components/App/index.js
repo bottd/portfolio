d2e379306cc5cd6f23380a047ebab5684f23d762
Drake Bott <bott.da@gmail.com>
2019-03-05 13:32:40 -0700

    Add background image to app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
+import forest from '../../assets/forest.jpg';
 
 export default function App(props) {
   return (
-    <div className="App">
-      <h1 className="name">Drake Bott</h1>
-      <div className="color color-1"></div>
-      <div className="color color-2"></div>
-      <div className="color color-3"></div>
-      <div className="color color-4"></div>
-      <div className="color color-5"></div>
+    <div className="App" style={{ backgroundImage: `url(${forest})` }}>
+      <div className="title-container">
+        <h1 className="title-name">Drake Bott</h1>
+        <h2 className="title-tagline">Software Developer</h2>
+      </div>
     </div>
   );
 }
