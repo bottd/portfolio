dc87ff0e525529b20327863780ffa2e61e619dc1
Drake Bott <bott.da@gmail.com>
2019-03-05 11:47:15 -0700

    Save test changes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,7 +4,12 @@ import './App.css';
 export default function App(props) {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="name">Drake Bott</h1>
+      <div className="color color-1"></div>
+      <div className="color color-2"></div>
+      <div className="color color-3"></div>
+      <div className="color color-4"></div>
+      <div className="color color-5"></div>
     </div>
   );
 }
