acb8e22b9480597fdbd42a2171c5de45796b8cc6
Drake Bott <bott.da@gmail.com>
2019-03-06 16:05:46 -0700

    Import Title component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import forest from '../../assets/forest.jpg';
+import Title from '../Title';
 
 export default function App(props) {
   return (
     <div className="App" style={{ backgroundImage: `url(${forest})` }}>
-      <div className="title-container">
-        <h1 className="title-name">Drake Bott</h1>
-        <h2 className="title-tagline">Software Developer</h2>
-      </div>
+      <Title />
     </div>
   );
 }
