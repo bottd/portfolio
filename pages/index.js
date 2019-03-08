6ab053460c99e11e90e6688e7b8386b721b6b8a4
Drake Bott <bott.da@gmail.com>
2019-03-08 00:14:14 -0700

    Prettier

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Title from '../components/Title';
+import Head from '../components/Head';
 
 export default function Home(props) {
   return (
     <div className="Home">
+      <Head />
       <Title />
       <style jsx global>
         {`
           body {
             margin: 0;
             padding: 0;
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
-              "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
-              sans-serif;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+              'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+              'Helvetica Neue', sans-serif;
             -webkit-font-smoothing: antialiased;
             -moz-osx-font-smoothing: grayscale;
           }
