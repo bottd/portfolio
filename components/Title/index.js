4b460be4cc6649a418bcb387e453a36c13554490
Drake Bott <bott.da@gmail.com>
2019-03-07 23:44:40 -0700

    Move CSS to Title component file

--- components/Title/index.js
+++ components/Title/index.js
@@ -1,16 +1,52 @@
 import React from 'react';
-import './Title.css';
-import forest from '../../assets/forest.jpg';
-import orange from '../../assets/orange.jpg';
 
 export default function Title(props) {
   return (
-    <div className="Title" style={{ backgroundImage: `url(${orange})` }}>
+    <div className="Title" style={{backgroundImage: `url("/static/orange.jpg")`}}>
       <div className="Title-container">
         <h1 className="Title-name">Drake Bott</h1>
-        <hr></hr>
+        <hr />
         <h2 className="Title-tagline">Software Developer</h2>
       </div>
+      <style jsx>
+        {`
+          .Title {
+            /*animation: 3s background-slide;*/
+            background-image: url("/static/orange.jpg")
+            box-sizing: border-box;
+            width: 100%;
+            height: 100vh;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+          }
+
+          .Title-container {
+            font-family: 'Lobster', cursive;
+            display: flex;
+            flex-direction: column;
+            color: #f49032;
+            margin: 0;
+            margin-top: 21vh;
+            text-align: center;
+            width: 50%;
+          }
+
+          hr {
+            width: 70%;
+          }
+
+          .Title-name {
+            font-size: 8em;
+            margin: 0;
+          }
+
+          .Title-tagline {
+            font-size: 4em;
+            margin: 0;
+          }
+        `}
+      </style>
     </div>
   );
 }
