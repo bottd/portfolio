0454f381b0afa324becd4a94c18c202b44ea1773
Drake Bott <bott.da@gmail.com>
2019-03-08 15:08:41 -0700

    Create about component

---
+++ components/About/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function About(props) {
+  return (
+    <div className="About">
+      <h1>about</h1>
+      <style jsx>
+        {`
+        .About {
+          height: 100vh;
+          width: 100%;
+        }
+        `}
+      </style>
+    </div>
+  );
+}
