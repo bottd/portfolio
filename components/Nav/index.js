3fa4f1ac103011dc14a5d792a7e486e249ae9eed
Drake Bott <bott.da@gmail.com>
2019-03-11 14:24:28 -0600

    Create Nav component

---
+++ components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Nav(props) {
+  return (
+    <div className="Nav">
+      <p>Drake Bott</p>
+      <p>About</p>
+      <p>Projects</p>
+      <p>Resume</p>
+      <style jsx>{``}</style>
+    </div>
+  );
+}
