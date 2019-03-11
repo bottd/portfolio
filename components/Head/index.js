25753870caf7ddf6090ba6771e57734cf8695d3d
Drake Bott <bott.da@gmail.com>
2019-03-11 14:24:17 -0600

    Add viewport meta tag to head

--- components/Head/index.js
+++ components/Head/index.js
@@ -5,6 +5,7 @@ export default function Head(props) {
   return (
     <NextHead>
       <title>Drake</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link href="/static/normalize.css" rel="stylesheet" />
     </NextHead>
   );
