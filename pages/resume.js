da033592410aae46e39ef0501a04aa0eb6573b81
Drake Bott <bott.da@gmail.com>
2019-03-13 00:59:49 -0600

    Remove unused Head tag

--- pages/resume.js
+++ pages/resume.js
@@ -4,7 +4,6 @@ import Layout from '../components/Layout';
 export default function Resume(props) {
   return (
     <Layout className="Resume" title="Drake: Resume">
-      <Head />
       <style jsx>{``}</style>
     </Layout>
   );
