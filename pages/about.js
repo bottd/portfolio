ad6e694c8db99bc74ef6a3f20940d68c0560151d
Drake Bott <bott.da@gmail.com>
2019-03-12 20:18:23 -0600

    Create about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Head from '../components/Head';
+
+export default function (props) {
+  return (
+    <div className="About">
+      <Head />
+      <style jsx>{``}</style>
+    </div>
+  );
+}
