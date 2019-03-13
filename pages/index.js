65e123b73230cee6c9f68971d143e74da63433f6
Drake Bott <bott.da@gmail.com>
2019-03-12 20:18:59 -0600

    Remove global body styles from page

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import Title from '../components/Title';
 import Head from '../components/Head';
-import About from '../components/About';
 
 export default function Home(props) {
   return (
     <div className="Home">
       <Head />
       <Title />
-      <style jsx global>
+      <style jsx>
         {`
-          body {
-            margin: 0;
-            padding: 0;
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
-              'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
-              'Helvetica Neue', sans-serif;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-          }
           .Home {
             display: flex;
             align-items: center;
