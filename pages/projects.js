f1ce06bd205268e40e70bf8fbf983df0701db5f5
Drake Bott <bott.da@gmail.com>
2019-03-12 20:18:31 -0600

    Create projects page

---
+++ pages/projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from '../components/Head';
+import About from '../components/About';
+
+export default function Projects(props) {
+  return (
+    <div className="Projects">
+      <Head />
+      <About />
+      <style jsx>
+        {``}
+      </style>
+    </div>
+  );
+}
