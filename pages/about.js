a9ae27f8718b5eb71d5edf09614ce7a99d3284d0
Drake Bott <bott.da@gmail.com>
2019-03-13 00:20:50 -0600

    Add styling to About page

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Head from '../components/Head';
 
-export default function (props) {
+export default function(props) {
   return (
-    <div className="About">
+    <div
+      className="About"
+      style={{ backgroundImage: `url("/static/orange.jpg")` }}>
       <Head />
-      <style jsx>{``}</style>
+      <style jsx>{`
+        .About {
+          box-sizing: border-box;
+          width: 100%;
+          height: 100vh;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+        }
+      `}</style>
     </div>
   );
 }
