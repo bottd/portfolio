9f89a88a96c79c35138739a205f789b41067f8b9
Drake Bott <bott.da@gmail.com>
2019-03-13 00:46:22 -0600

    Fix Title CSS issue

--- components/Title/index.js
+++ components/Title/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 export default function Title(props) {
   return (
-    <div className="Title" style={{backgroundImage: `url("/static/orange.jpg")`}}>
+    <div
+      className="Title"
+      style={{ backgroundImage: `url("/static/orange.jpg")` }}>
       <div className="Title-container">
         <h1 className="Title-name">Drake Bott</h1>
         <hr />
@@ -20,17 +22,19 @@ export default function Title(props) {
           }
 
           .Title-container {
-            font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue, sans-serif;
             display: flex;
-            flex-direction: column;
             color: #fff;
-            margin: 0;
-            margin-top: 22vh;
+            flex-direction: column;
+            position: absolute;
             text-align: center;
+            top: 22vh;
             width: 50%;
           }
 
           hr {
+            color: #fff;
             width: 60%;
             margin: 0 auto;
           }
