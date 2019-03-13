8984feefb0ca9ad48f22d6ec64c116977d9996bf
Drake Bott <bott.da@gmail.com>
2019-03-12 20:21:39 -0600

    Move body styles into Head component

--- components/Head/index.js
+++ components/Head/index.js
@@ -7,6 +7,19 @@ export default function Head(props) {
       <title>Drake</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link href="/static/normalize.css" rel="stylesheet" />
+      <style jsx global>
+        {`
+          body {
+            margin: 0;
+            padding: 0;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+              'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+              'Helvetica Neue', sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+        `}
+      </style>
     </NextHead>
   );
 }
