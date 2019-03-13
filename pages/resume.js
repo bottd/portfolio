a801bbead0a9574b414cffe32bcf32bd63d43301
Drake Bott <bott.da@gmail.com>
2019-03-12 20:18:39 -0600

    Create resume page

---
+++ pages/resume.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from '../components/Head';
+
+export default function Resume(props) {
+  return (
+    <div className="Resume">
+      <Head />
+      <style jsx global>
+        {``}
+      </style>
+    </div>
+  );
+}
