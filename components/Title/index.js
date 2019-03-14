8ff03fbbf0d6d5cada46a7cbd296984f814d575f
Drake Bott <bott.da@gmail.com>
2019-03-14 01:20:42 -0600

    Merge social elements into Title component

--- components/Title/index.js
+++ components/Title/index.js
@@ -1,19 +1,42 @@
 import React from 'react';
+import Social from '../Social';
 
-export default function Title(props) {
+export default function Title({ scrollRef }) {
+  function scrollToRef() {
+    scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
     <div
       className="Title"
       style={{ backgroundImage: `url("/static/orange.jpg")` }}>
+      <div className="social-container">
+        <a href="https://github.com/bottd/">
+          <i class="fab fa-github" />
+        </a>
+        <a href="https://www.linkedin.com/in/bottd/">
+          <i class="fab fa-linkedin" />
+        </a>
+        <a href="mailto:drake@drake.dev">
+          <i class="fas fa-envelope" />
+        </a>
+      </div>
       <div className="Title-container">
         <h1 className="Title-name">Drake Bott</h1>
         <hr />
         <h2 className="Title-tagline">Web developer in Denver, CO</h2>
       </div>
+      <div className="btn-container">
+        <button className="Title-scroll" onClick={scrollToRef}>
+          <i class="fas fa-chevron-down" />
+        </button>
+      </div>
       <style jsx>
         {`
           .Title {
+            positon: relative;
             box-sizing: border-box;
+            color: #fff;
             width: 100%;
             height: 100vh;
             background-position: center;
@@ -21,11 +44,26 @@ export default function Title(props) {
             background-size: cover;
           }
 
+          .social-container {
+            align-items: center;
+            background-color: transparent;
+            display: flex;
+            font-size: 2em;
+            position: absolute;
+            justify-content: flex-end;
+            top: 0;
+            width: 100%;
+            height: 6vh;
+          }
+          a {
+            margin: 0 1% 0 1%;
+            color: #fff;
+          }
+
           .Title-container {
             font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
               Oxygen-Sans, Ubuntu, Cantarell, Helvetica Neue, sans-serif;
             display: flex;
-            color: #fff;
             flex-direction: column;
             position: absolute;
             text-align: center;
@@ -35,7 +73,7 @@ export default function Title(props) {
 
           hr {
             color: #fff;
-            width: 60%;
+            width: 31em;
             margin: 0 auto;
           }
 
@@ -47,6 +85,27 @@ export default function Title(props) {
           .Title-tagline {
             font-size: 2em;
             margin-top: 1vh;
+          }
+
+          .btn-container {
+            position: absolute;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            top: 95vh;
+            width: 100%;
+          }
+
+          .Title-scroll {
+            background: none;
+            border: none;
+            color: #fff;
+            font-size: 3em;
+          }
+
+          .Title-scroll:focus {
+            outline: 0;
           }
         `}
       </style>
