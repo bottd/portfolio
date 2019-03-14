bb8128d98f26c0d3d3a1c6f7fe56fa51f77ec412
Drake Bott <bott.da@gmail.com>
2019-03-14 01:20:25 -0600

    remove nav component from Layout

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Head from '../Head';
-import Nav from '../Nav';
 import Footer from '../Footer';
 
 export default function Layout({ children, className, title = 'Drake' }) {
   return (
     <div className={className}>
       <Head title={title} />
-      <Nav />
       {children}
       <Footer />
     </div>
