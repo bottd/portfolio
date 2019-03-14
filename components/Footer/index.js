dcffaf15068b87115e259198b3232a1ab94f0b04
Drake Bott <bott.da@gmail.com>
2019-03-13 23:38:56 -0600

    Create footer component

---
+++ components/Footer/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function Footer(props) {
+  return (
+    <div className="Footer">
+      <a href="https://github.com/bottd/">
+        <i class="fab fa-github" />
+      </a>
+      <a href="https://www.linkedin.com/in/bottd/">
+        <i class="fab fa-linkedin" />
+      </a>
+      <a href="mailto:drake@drake.dev">
+        <i class="fas fa-envelope" />
+      </a>
+      <style jsx>
+        {`
+          .Footer {
+            align-items: center;
+            background-color: #353535;
+            display: flex;
+            font-size: 2.5em;
+            justify-content: center;
+            width: 100%;
+            height: 10vh;
+          }
+          a {
+            margin: 0 2% 0 2%;
+            color: #fff;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
