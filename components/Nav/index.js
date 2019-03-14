f2a920a352985f51bf0e0f1ea3f616c18c577dd4
Drake Bott <bott.da@gmail.com>
2019-03-13 23:39:54 -0600

    Add social link buttons

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -3,11 +3,34 @@ import React from 'react';
 export default function Nav(props) {
   return (
     <div className="Nav">
-      <p>Drake Bott</p>
-      <p>About</p>
-      <p>Projects</p>
-      <p>Resume</p>
-      <style jsx>{``}</style>
+      <a href="https://github.com/bottd/">
+        <i class="fab fa-github" />
+      </a>
+      <a href="https://www.linkedin.com/in/bottd/">
+        <i class="fab fa-linkedin" />
+      </a>
+      <a href="mailto:drake@drake.dev">
+        <i class="fas fa-envelope" />
+      </a>
+      <style jsx>
+        {`
+          .Nav {
+            align-items: center;
+            background-color: transparent;
+            display: flex;
+            font-size: 2em;
+            position: absolute;
+            justify-content: flex-end;
+            top: 0;
+            width: 100%;
+            height: 6vh;
+          }
+          a {
+            margin: 0 1% 0 1%;
+            color: #fff;
+          }
+        `}
+      </style>
     </div>
   );
 }
