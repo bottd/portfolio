de37537fd4e9ea51bbf6f82d63c552e3042fdb2f
Drake Bott <bott.da@gmail.com>
2019-03-28 14:03:07 -0600

    Add resource links to icons

--- components/About/index.js
+++ components/About/index.js
@@ -27,65 +27,81 @@ const icons = [
   {
     title: 'Chai',
     url: '/static/images/chai.svg',
+    link: 'https://www.chaijs.com/',
   },
   {
     title: 'CSS',
     url: '/static/images/css-3.svg',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/CSS',
   },
   {
     title: 'Docker',
     url: '/static/images/docker.svg',
+    link: 'https://www.docker.com/',
   },
   {
     title: 'Discord',
     url: '/static/images/discord.svg',
+    link: 'https://discordapp.com/developers/docs/intro',
   },
   {
     title: 'Git',
     url: '/static/images/git.svg',
+    link: 'https://git-scm.com/',
   },
   {
     title: 'GraphQL',
     url: '/static/images/graphql.svg',
+    link: 'https://graphql.org/',
   },
   {
     title: 'HTML',
     url: '/static/images/html-5.svg',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
   },
   {
     title: 'JavaScript',
     url: '/static/images/javascript.svg',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
   },
   {
     title: 'Jest',
     url: '/static/images/jest.svg',
+    link: 'https://jestjs.io/',
   },
   {
     title: 'Mocha',
     url: '/static/images/mocha.svg',
+    link: 'https://mochajs.org/',
   },
   {
     title: 'Node',
     url: '/static/images/node.svg',
+    link: 'https://nodejs.org/en/',
   },
   {
     title: 'NPM',
     url: '/static/images/npm-2.svg',
+    link: 'https://www.npmjs.com/',
   },
   {
     title: 'PostgreSQL',
     url: '/static/images/postgresql.svg',
+    link: 'https://www.postgresql.org/',
   },
   {
     title: 'React',
     url: '/static/images/React.svg',
+    link: 'https://reactjs.org/',
   },
   {
     title: 'Redux',
     url: '/static/images/redux.svg',
+    link: 'https://redux.js.org/',
   },
   {
     title: 'Vim',
     url: '/static/images/vim.svg',
+    link: 'https://www.vim.org/',
   },
 ];
