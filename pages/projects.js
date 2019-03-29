f512aeecfa6565858cb8f20db8f61daba54aad54
Drake Bott <bott.da@gmail.com>
2019-03-29 15:52:39 -0600

    Import project component

--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Project from '../components/Project';
 
 export default function Projects(props) {
+  const displayProjects = projects.map(project => <Project {...project}/>);
   return (
     <Layout className="Projects" title="Drake: Projects">
+      {displayProjects}
       <style jsx>{``}</style>
     </Layout>
   );
