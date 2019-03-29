fbb652d80f8c6c17d54bfb98944589d2da0c2a93
Drake Bott <bott.da@gmail.com>
2019-03-29 14:20:34 -0600

    Add nav bar to layout

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Head from '../Head';
+import Navigation from '../Navigation';
 import Footer from '../Footer';
 
 export default function Layout({ children, className, title = 'Drake' }) {
   return (
     <div className={className}>
       <Head title={title} />
+      <Navigation />
       {children}
       <Footer />
     </div>
