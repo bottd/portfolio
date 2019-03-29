c39b834e95eb5b352e46e3ded3d7af0be201e2fe
Drake Bott <bott.da@gmail.com>
2019-03-29 14:20:19 -0600

    Create nav bar

---
+++ components/Navigation.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Navigation(props) {
+  return (
+    <nav className="Navigation">
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/projects">
+        <a>Projects</a>
+      </Link>
+      <Link href="/contact">
+        <a>Contact</a>
+      </Link>
+      <style jsx>
+        {`
+        .Navigation {
+          align-items: center;
+          background-color: #353535;
+          display: flex;
+          margin: 0;
+          position: fixed;
+          height: 8vh;
+          width: 100%;
+          z-index: -1;
+        }
+        a {
+          color: #fff;
+          font-size: 1.5em;
+          margin-left: 2.5%;
+          margin-right: 2.5%;
+          text-decoration: none;
+        }
+        a:visited {
+          color: #fff;
+        }
+        `}
+      </style>
+    </nav>
+  )
+}
